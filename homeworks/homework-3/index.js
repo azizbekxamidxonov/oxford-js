8e14f58055c744c45caba63f568dbdd30d293ad4
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-10 14:34:51 +0500

    homework finished

---
+++ homeworks/homework-3/index.js
@@ -0,0 +1,18 @@
+// let a = 1, b = 1;
+
+// let c = ++ a; // 1ni qadam kotarish  kotarish  natija2 va a 1 ni qoshish
+// let d = b ++;  //1 ozi keyin 1 qadam kotarish 2 natija  
+
+// console.log(++ a);
+// console.log(b ++);
+
+
+
+// let a = 2
+// let x = 1 + (a*=2)
+
+// console.log(1+(a*=2)) bunisini tushunmadim
+
+ 
+
+
---
+++ lessons/1-dars/ndex.js
@@ -0,0 +1,5 @@
+let a = 12
+let b = 19
+let c = 9
+let e = 5
+console.log(a + (b - c)*e)
