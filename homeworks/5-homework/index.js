f95c5c71e3c1fd4d4f89b75d329228c947c62b51
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-13 14:03:11 +0500

    finished homewrowk

---
+++ homeworks/5-homework/index.js
@@ -0,0 +1,64 @@
+// let a = 5;
+// let b = 10;
+
+// a = +a;
+// b = +a;
+
+// if (a == b) {
+//   a = 0;
+// } else {
+//   a = 0;
+//   b = 0;
+// }
+
+// console.log(a, b);
+
+
+// let a = 7;
+// let b = 12;
+
+
+// if (a == b) {
+//   a = 0;
+//   b = 0;
+// } else {
+//   if (a > b) {
+//     b = a;
+//   } else {
+//     a = b;
+//   }
+// }
+
+// console.log(a, b);
+
+// let a = 20;
+// let b = 8;
+// let c = 15;
+
+// let natija;
+
+// if (a <= b && a <= c) {
+//   natija = a;
+// } else if (b <= a && b <= c) {
+//   natija = b;
+// } else {
+//   natija = c;
+// }
+
+// console.log(natija);
+
+// let x = 9;
+// let y = 15;
+// let z = 12;
+
+// let natija;
+
+// if ((x > y && x < z) || (x > z && x < y)) {
+//   natija = x;
+// } else if ((y > x && y < z) || (y > z && y < x)) {
+//   natija = y;
+// } else {
+//   natija = z;
+// }
+
+// console.log(natija);
---
+++ lessons/5-dars/index.js
@@ -0,0 +1,19 @@
+// let number = 0
+
+// if (number < 0) {
+//     --number
+//     --number
+// }
+// else if (number >= 5) {
+//     number++
+// }
+
+// else if(number=10)
+
+// else {
+//     console.log("Siz 0 dan katta yoki kichik son kiriting!!!");
+    
+// }
+// console.log(number);
+
+
