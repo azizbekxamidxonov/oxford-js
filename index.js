44ea778e2f2dec6b71ccc96c11f6e95191fab18c
azizbekxamidxonov <assalomualeykum2007@gmail.com>
2025-05-17 13:37:41 +0500

    finished homework

--- index.js
+++ index.js
@@ -15,11 +15,8 @@
 //   }
 // }
 
-// for (let i = 0; i < 5; i++ ) natija 0,1,2,3,4 chiqida sabab i<5 dan deyilga 
-// i++ qadamni birga oshiradi 5ga borganda esa toxtaydi 5 dan  kichik bolsin deyilgan shartda 
-//     console.log(i);
     
 
-// for ( let i = 0; i < 5; ++i ) bunda ham xuddi shunday xolat bajariladi 
+// for ( let i = 0; i < 5; ++i ) bunda ham xuddi shunday xolatda  bajariladi.
 //      console.log(i);
     
